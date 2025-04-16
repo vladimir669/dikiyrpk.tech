084be4f00a6ce307bd446cabfad0f432327685b3
vladimir669 <vovanxd9955@gmail.com>
2025-04-16 16:59:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-TELEGRAM_BOT_TOKEN = "7937013933:AAF_iuBecx-o0etgGZhEzWGxv3cBHWfDpYQ"
-TELEGRAM_GROUP_ID = "-1002633190524"
-
-# Пароли
-USER_PASSWORD = "4444"   # Пароль для входа на сайт
-ADMIN_PASSWORD = "880088" # Пароль для входа в админку
+ADMIN_PASSWORD = "880088"
