6eb64b08ebd9061e1c776a5ba3856e8133591f0a
vladimir669 <vovanxd9955@gmail.com>
2025-04-16 18:03:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
-ADMIN_PASSWORD = "880088"
+# Пароль для обычных пользователей
+USER_PASSWORD = "1234"
+
+# Пароль для администратора
+ADMIN_PASSWORD = "admin"
+
+# Telegram Bot API Token
+BOT_TOKEN = '7937013933:AAF_iuBecx-o0etgGZhEzWGxv3cBHWfDpYQ'
+
+# Telegram Group ID
+GROUP_ID = '-1002633190524'
